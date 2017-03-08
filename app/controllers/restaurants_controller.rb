70a24bd3fb53f7bf00a8073fcbdcba570696096b
Heninapso <heninapso@gmail.com>
2017-03-08 13:35:48 +0100

    add restaurant method added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,5 +4,25 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def show
+    @restaurant = Restaurant.find(restaurant_params)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
 
